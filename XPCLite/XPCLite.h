74acc9492f35996514c48aa09282562a4459db79
lemon4ex <netchen@vip.qq.com>
2020-11-13 01:37:50 +0800

    Initial Commit

---
+++ XPCLite/XPCLite.h
@@ -0,0 +1,19 @@
+//
+//  XPCLite.h
+//  XPCLite
+//
+//  Created by h4ck on 2020/11/13.
+//  Copyright © 2020 猿码工作室（https://ymlab.net）. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XPCLite.
+FOUNDATION_EXPORT double XPCLiteVersionNumber;
+
+//! Project version string for XPCLite.
+FOUNDATION_EXPORT const unsigned char XPCLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XPCLite/PublicHeader.h>
+
+
